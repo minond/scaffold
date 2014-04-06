939c098e48503ca09ba60c085ba024ab3955985e
Marcos Minond <minond.marcos@gmail.com>
2014-04-06 15:35:25 -0600

    adding mocha tests, cleaning up exec option definitions, adding binaries section to config file

--- grunt/options/exec.js
+++ grunt/options/exec.js
@@ -1,25 +1,75 @@
+/**
+ * array join helper
+ * @param {string} strings*
+ * @return {string} joined string
+ */
+function join () {
+    return [].splice.call(arguments, 0).join(' ')
+}
+
 module.exports = {
     scsslint: {
-        cmd: 'scss-lint -f XML <%= config.files.sass.dir %> > <%= config.artifacts.linters.scsslint %>'
+        cmd: join(
+            '<%= config.binaries.scsslint %>',
+            '-f XML <%= config.files.sass.dir %>',
+            '>',
+            '<%= config.artifacts.linters.scsslint %>'
+        )
     },
     phpmd: {
-        // removing controversial because of camelCame bs
-        // cleancode rule not in 1.4.*
-        cmd: 'bin/phpmd <%= config.files.php.dir %>,<%= config.files.tests.dir %> xml codesize,design,naming,unusedcode --reportfile <%= config.artifacts.linters.phpmd %> --strict'
+        cmd: join(
+            '<%= config.binaries.phpmd %>',
+            '<%= config.files.php.dir %>,<%= config.files.tests.dir %>',
+            'xml',
+            // removing controversial because of camelCame bs
+            // cleancode rule not in 1.4.*
+            'codesize,design,naming,unusedcode',
+            '--reportfile <%= config.artifacts.linters.phpmd %>',
+            '--strict'
+        )
     },
     phpcpd: {
-        cmd: 'bin/phpcpd <%= config.files.php.all %> <%= config.files.tests.dir %>'
+        cmd: join(
+            '<%= config.binaries.phpmd %>',
+            '<%= config.files.php.all %>',
+            '<%= config.files.tests.dir %>'
+        )
     },
     phpdcd: {
-        cmd: 'bin/phpdcd <%= config.files.php.all %> <%= config.files.tests.dir %>'
+        cmd: join(
+            '<%= config.binaries.phpmd %>',
+            '<%= config.files.php.all %>',
+            '<%= config.files.tests.dir %>'
+        )
     },
     apigen: {
-        cmd: 'apigen -s=<%= config.files.php.dir %> -d=<%= config.artifacts.documentation.apigen %> --todo=yes --colors=no --progressbar=no'
+        cmd: join(
+            '<%= config.binaries.apigen %>',
+            '-s=<%= config.files.php.dir %>',
+            '-d=<%= config.artifacts.documentation.apigen %>',
+            '--todo=yes',
+            '--colors=no',
+            '--progressbar=no'
+        )
     },
     phantomjs: {
-        cmd: 'node_modules/.bin/phantomjs --webdriver=8643'
+        cmd: join(
+            '<%= config.binaries.phantomjs %>',
+            '--webdriver=8643'
+        )
     },
     behat: {
-        cmd: 'bin/behat --format junit,html,pretty --out <%= config.artifacts.tests.behat.junit %>,<%= config.artifacts.tests.behat.html %>'
+        cmd: join(
+            '<%= config.binaries.behat %>',
+            '--format junit,html,pretty',
+            '--out <%= config.artifacts.tests.behat.junit %>,<%= config.artifacts.tests.behat.html %>'
+        )
+    },
+    mocha: {
+        cmd: join(
+            '<%= config.binaries.mocha %>',
+            '<%= config.files.tests.dir %>',
+            '--reporter spec'
+        )
     }
 };
