5f58f46e5cffb70574084cda4ce35ba9fa3e4a71
Marcos Minond <minond.marcos@gmail.com>
2014-11-19 23:53:13 -0700

    adding jscs to js plugin and normalizing target names

--- plugins/js.mk
+++ plugins/js.mk
@@ -9,29 +9,40 @@ JS_TESTS_OUTPUT = build/tests/js/report
 JS_COMPLEXITY_FLAGS = --format markdown
 JS_COMPLEXITY_FILES = $(JS_FILES)
 
-JS_HINT_FLAGS = --config $(CONFIG_DIR)/jshintrc.json
-JS_HINT_FILES = $(JS_FILES) $(JS_TESTS)
+JS_JSHINT_FLAGS = --config $(CONFIG_DIR)/jshintrc.json
+JS_JSHINT_FILES = $(JS_FILES) $(JS_TESTS)
 
-.PHONY: js-configure js-complexity js-test js-test-coverage js-hint js-coveralls
+JS_JSCS_FILES = $(JS_FILES) $(JS_TESTS)
+
+.PHONY: js-configure js-complexity js-test js-test-coverage js-hint \
+    js-coveralls js-jscs
 
 js-configure:
 	$(NPM) i --save-dev istanbul@0.3.2
 	$(NPM) i --save-dev mocha@2.0.1
 	$(NPM) i --save-dev complexity-report@1.0.6
 	$(NPM) i --save-dev jshint@2.5.10
+	$(NPM) i --save-dev jscs@1.7.3
 
-js-hint:
-	$(NPM_BIN)/jshint $(JS_HINT_FLAGS) $(JS_HINT_FILES)
+js-jscs:
+	$(NPM_BIN)/jscs $(JS_JSCS_FILES)
+
+js-jshint:
+	$(NPM_BIN)/jshint $(JS_JSHINT_FLAGS) $(JS_JSHINT_FILES)
 
 js-complexity:
 	$(NPM_BIN)/cr $(JS_COMPLEXITY_FLAGS) $(JS_COMPLEXITY_FILES)
 
-js-test:
+js-mocha:
 	$(NPM_BIN)/mocha $(JS_TESTS)
 
-js-test-coverage:
+js-mocha-coverage:
 	$(NPM_BIN)/istanbul cover $(NPM_BIN)/_mocha --report lcov \
 		--dir $(JS_TESTS_OUTPUT) -- -R spec $(JS_TESTS) --recursive
 
 js-coveralls:
 	cat $(JS_TESTS_OUTPUT)/lcov.info | $(NPM_BIN)/coveralls
+
+js-test: js-mocha
+js-test-coverage: js-mocha-coverage
+js-lint: js-complexity js-jshint js-jscs
