5875a545b25e49eac71ac80091b7c9dc7114297e
Marcos Minond <minond.marcos@gmail.com>
2014-11-25 18:17:15 -0700

    creating vars for minifiers

--- plugins/minify.mk
+++ plugins/minify.mk
@@ -2,5 +2,9 @@ include plugins/base.mk
 
 .PHONY: minify-cofigure
 
+JSMIN = $(NPM_BIN)/jsmin
+CSSMIN = $(NPM_BIN)/cssmin
+HTMLMIN = $(NPM_BIN)/htmlmin
+
 minify-cofigure:
 	$(NPM) i --save jsmin cssmin htmlmin
