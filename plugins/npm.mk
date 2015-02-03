0ccf7c1a66e861d8cc5c9c2fdb94ab76b60c2049
Marcos Minond <minond.marcos@gmail.com>
2015-02-02 20:47:52 -0700

    adding npm plugin help

--- plugins/npm.mk
+++ plugins/npm.mk
@@ -7,5 +7,13 @@ SCAFFOLDED_NPM = 1
 npm ?= npm
 npm_bin ?= node_modules/.bin
 
+help::
+	@echo
+	@echo "npm:"
+	@echo "  npm-install     # runs npm install"
+	@echo
+	@echo "  \$$npm            # npm binary ($(npm))"
+	@echo "  \$$npm_bin        # node module binaries ($(npm_bin))"
+
 npm-install:
 	@npm install
